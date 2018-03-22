5efb07f93fc990d17c3fe8eed36e88fb6aa8487c
Guillermo Gomez <guillermo.gomez.pernas@gmail.com>
2018-03-22 12:57:12 +0000

    Update CameraLauncher.java
    changed captureDate from string to long

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -646,7 +646,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
                     ori_dimensions.put("height", options.outHeight + "");
                     original.put("dimensions", ori_dimensions );
                     original.put("size", file.length() );
-                    original.put("captureDate", filename );
+                    original.put("captureDate", System.currentTimeMillis() );
                     
                     thumbnail.put("path", tb_uri.toString() );
                     JSONObject tb_dimensions = new JSONObject();
@@ -654,7 +654,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
                     tb_dimensions.put("height", tb_options.outHeight + "");
                     thumbnail.put("dimensions", tb_dimensions );
                     thumbnail.put("size", tb_file.length() );
-                    thumbnail.put("captureDate", filename );
+                    thumbnail.put("captureDate", System.currentTimeMillis() );
 
                     success.put("original",original);
                     success.put("thumbnail",thumbnail);
